da364d89ee7fe4611f22a02a9683ca0197bef921
bmp-weka <ben.peterson@weka.io>
2025-10-07 15:22:46 -0600

    update zoom js

--- source/_static/zoom.js
+++ source/_static/zoom.js
@@ -1,10 +1,9 @@
-javascript
 document.addEventListener('DOMContentLoaded', function () {
   document.querySelectorAll('img.zoomable').forEach(function (img) {
     img.style.cursor = 'zoom-in';
     img.addEventListener('click', function () {
-      // Open the *built* image URL (what Sphinx put in src) in a new tab
-      window.open(img.currentSrc || img.src, '_blank');
+      // Open in a new window (instead of replacing current tab)
+      window.open(img.currentSrc || img.src, '_blank', 'noopener,noreferrer');
     });
   });
 });
